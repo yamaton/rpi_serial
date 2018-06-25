b175c84a2c337603cdbe3d6841769349738d6cf2
yamaton <yamaton@gmail.com>
2018-06-25 03:42:40 -0400

    Add comments on permission setting

--- src/serial_reader.py
+++ src/serial_reader.py
@@ -5,25 +5,31 @@
 
 In Linux environment, run following.
 ```shell
-$ python serial_reader.py
+python serial_reader.py
 ```
 
+## Permission handling
+If you have a permission error in accessing `PORT`, you need to add your user to `dialout` group (typically).
+
+```shell
+sudo usermod -a -G dialout "$USER"
+```
 """
 import serial
 
+PORT = "/dev/ttyUSB0"
 BAUD_RATE = 115200
 
 
 def main():
     ser = serial.Serial(
-        port="/dev/ttyUSB0",
+        port=PORT,
         baudrate=BAUD_RATE,
-        parity=serial.PARITY_NONE,
-        stopbits=serial.STOPBITS_ONE,
-        bytesize=serial.EIGHTBITS,
+        # parity=serial.PARITY_NONE,
+        # stopbits=serial.STOPBITS_ONE,
+        # bytesize=serial.EIGHTBITS,
         timeout=1,
     )
-
     while True:
         x = ser.readline()
         print(x)
